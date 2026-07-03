f33de057f28cf761dbf39e5fc4a5c8336056d38b
tam-04jinx <157656527+tam-04jinx@users.noreply.github.com>
2026-07-03 17:21:31 -0500

    Test timezone API

--- src/test/java/com/example/datetime/DateTimeControllerTest.java
+++ src/test/java/com/example/datetime/DateTimeControllerTest.java
@@ -1,7 +1,8 @@
 package com.example.datetime;
 
-import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.blankOrNullString;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.not;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,5 +26,16 @@ class DateTimeControllerTest {
                 .andExpect(jsonPath("$.time", not(blankOrNullString())))
                 .andExpect(jsonPath("$.dateTime", not(blankOrNullString())))
                 .andExpect(jsonPath("$.timeZone", not(blankOrNullString())));
+    }
+
+    @Test
+    void shouldReturnMultipleTimeZones() throws Exception {
+        mockMvc.perform(get("/api/timezones"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()", greaterThanOrEqualTo(6)))
+                .andExpect(jsonPath("$[0].city", not(blankOrNullString())))
+                .andExpect(jsonPath("$[0].zoneId", not(blankOrNullString())))
+                .andExpect(jsonPath("$[0].date", not(blankOrNullString())))
+                .andExpect(jsonPath("$[0].time", not(blankOrNullString())));
     }
 }
